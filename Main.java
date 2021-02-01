554343cdcde796f4691adf0745177b518bdfc138
ryanm100 <ryan.milton@patelhs.org>
2021-02-01 15:24:51 +0000

    I have added my statements. Aditionally, my scanner is now present too.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
+
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What's your current weight on Earth?");
+    int earthWeight = scan.nextInt();
     
   }
 }
