74d01f0db2872a75a0371e3a6c68b3b138ade990
ryanm100 <ryan.milton@patelhs.org>
2021-02-04 18:48:06 +0000

    Restarted my code, now asking for people info

--- Main.java
+++ Main.java
@@ -1,31 +1,22 @@
-//Ryan Milton
-//Feburary 1st 2021 
-//Period 3 Programming Gaber
-
-//Purpose of this program: You will be entering your current weight, and finding out your weight on the moon. This is so in case we someday travel to the moon you would have a rough estimate of what your weight would be. 
-
 import java.util.Scanner;
 
-class Main {
+public class Main {
 
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args); 
     Scanner scan = new Scanner(System.in);
+    {
+      System.out.println("What grade are you in (USA)");
+      String grade = scan.next;
+      System.out.println("Cool, now what is your age?");
+      String age = scan.next;
+      System.out.println("Last question, what's your name?");
+      String name = scan.next; 
+      System.out.println("Hello! Just to confirm you are " + name + age + grade);
 
-    System.out.println("What's your current weight on Earth?");
-    int earthWeight = scan.nextInt();
-    int moonWeight = scan.nextInt();
 
-    int weight = getWeight(earthWeight, currentYear);
-    System.out.println("your current weight is " + weight);
-
-     //This is a method to calculate and identify your current weight
-
-  static int getAge(int weight);
-  {
-    int weight = currentWeight;
-    return weight;
-  }
+    }
+    
+    
 //The way this works is by returning the weight of your current weight so that you will then calculate your weight on the moon
 
   }
