acc6962525223b1379c65ffc679d0d95ad27352b
ryanm100 <ryan.milton@patelhs.org>
2021-02-06 03:55:52 +0000

    Finished method 1

--- Main.java
+++ Main.java
@@ -10,16 +10,31 @@ import java.util.Scanner;
       //Above line is apart of my scanner 
       System.out.println("Are you in middle or high school?");
       //Question asking the user what grade they are in based on USA grade scale, as some other countries could have different grades based on their age, like the UK for example.
-      String grade = scan.next();
-      System.out.println("Hello! Just to confirm you are " + grade);
+      String userGrade = scan.next();
+      System.out.println("Hello! Just to confirm you are " + userGrade);
+      String output = grade(userGrade);
+      System.out.println(output);
+      String output2 = checkAge(yourAge);
+      System.out.println(output2);
       //This is printing their confirmation and the information that they gave. As the code is requesting the user for their name, age and grade,it will print that in which the user is pretty much confirming it.
-
-
-
-
-
-
-
+      //
     }
+      //Method 1:
+      static String grade(String grade){
+        System.out.println(grade);
+        if (grade.equals("middle")){
+          return("You're better in Algebra");
+        }else if (grade.equals("high")){
+          return("You are better off in Calc or Geometry as you're in high school");
+        }else return("Incorrect, try again");
+} 
+        //Method 2:
+        static String checkAge(String yourAge){
+          System.out.println(yourAge);
+          if (yourAge.equals)
+        }
   }
+
+    
+    
 
