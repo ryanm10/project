cb322462ac0e9b13f37b2c33b8c1270579538f38
Ryan <ryan.milton@patelhs.org>
2021-02-06 02:48:32 +0000

    Less questions now

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+//My project - If the user themself is in middle or high school, it will give them a suggestion on what math class they should be in right now.
+//If they are in middle school, Algebra will be required, however if they are in high school it will say Calculus or Geometry will be required for them.
 import java.util.Scanner;
 //Imported scanner **MUST**
 
@@ -6,20 +8,15 @@ import java.util.Scanner;
     {
       Scanner scan = new Scanner(System.in);
       //Above line is apart of my scanner 
-      System.out.println("What grade are you in (USA)");
+      System.out.println("Are you in middle or high school?");
       //Question asking the user what grade they are in based on USA grade scale, as some other countries could have different grades based on their age, like the UK for example.
       String grade = scan.next();
-      //Getting the user input 
-      System.out.println("Cool, now what is your age?");
-      //Asking for their age, just asking for basic information 
-      String age = scan.next();
-      //Getting the user input again
-      System.out.println("Last question, what's your name?");
-      //Asking for the user's name so that when they are in the "confirmation" screen, they will get a quick review of the information they had submitted.
-      String name = scan.next();
-      //User input 
-      System.out.println("Hello! Just to confirm you are " + name + age + grade);
+      System.out.println("Hello! Just to confirm you are " + grade);
       //This is printing their confirmation and the information that they gave. As the code is requesting the user for their name, age and grade,it will print that in which the user is pretty much confirming it.
+
+
+
+
 
 
 
