844232376d7d4b9d60af22d4e07a5fdb202d98c5
ryanm100 <ryan.milton@patelhs.org>
2021-02-06 02:30:09 +0000

    Added my scanner code also contains: - Question asking for grade - Question asking for age - Question asking for name
    And a final confirmation

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
-
-  public static void main(String[] args); 
-    Scanner scan = new Scanner(System.in);
+  class Main {
+    public static void main(String[] args) 
     {
+      Scanner scan = new Scanner(System.in);
+      //Above line is apart of my scanner 
       System.out.println("What grade are you in (USA)");
-      String grade = scan.next;
+      //Question asking the user what grade they are in based on USA grade scale, as some other countries could have different grades based on their age, like the UK for example.
+      String grade = scan.next();
+      //Getting the user input 
       System.out.println("Cool, now what is your age?");
-      String age = scan.next;
+      //Asking for their age, just asking for basic information 
+      String age = scan.next();
       System.out.println("Last question, what's your name?");
-      String name = scan.next; 
+      String name = scan.next(); 
       System.out.println("Hello! Just to confirm you are " + name + age + grade);
 
 
-    }
-    
-    
-//The way this works is by returning the weight of your current weight so that you will then calculate your weight on the moon
 
+    }
   }
-}
+
