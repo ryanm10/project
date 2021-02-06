7691ff8485b0ff65b99b9b245e5d5fcd03f929b1
Ryan <ryan.milton@patelhs.org>
2021-02-06 02:34:14 +0000

    Added comments regarding my inputs and what I'm asking the user.

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//Imported scanner **MUST**
 
   class Main {
     public static void main(String[] args) 
@@ -12,9 +13,13 @@ import java.util.Scanner;
       System.out.println("Cool, now what is your age?");
       //Asking for their age, just asking for basic information 
       String age = scan.next();
+      //Getting the user input again
       System.out.println("Last question, what's your name?");
-      String name = scan.next(); 
+      //Asking for the user's name so that when they are in the "confirmation" screen, they will get a quick review of the information they had submitted.
+      String name = scan.next();
+      //User input 
       System.out.println("Hello! Just to confirm you are " + name + age + grade);
+      //This is printing their confirmation and the information that they gave. As the code is requesting the user for their name, age and grade,it will print that in which the user is pretty much confirming it.
 
 
 
